fdd309baf9e647d0fae9bcca7b2c5b8d1dbaecf1
SiHun Sung <sungsihun@gmail.com>
2018-09-28 16:21:16 -0400

    add spinner in TeacherListVC

--- TutorApp/Controllers/StudentDetailViewController.swift
+++ TutorApp/Controllers/StudentDetailViewController.swift
@@ -9,237 +9,263 @@
 import UIKit
 
 class StudentDetailViewController: UIViewController {
-
-    // MARK: - Always Viewable Outlets
   
-    @IBOutlet weak var studentImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
+  // MARK: - Always Viewable Outlets
+  
+  @IBOutlet weak var studentImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var subjectLabel: UILabel!
+  @IBOutlet weak var backgroundView: UIView!
   
-    // MARK: - Homework Viewable Outlets
-    
-    @IBOutlet weak var assignmentsTableView: UITableView!
+  // MARK: - Homework Viewable Outlets
   
-    // MARK: - Action Methods
+  @IBOutlet weak var assignmentsTableView: UITableView!
   
-    @IBAction func addAssignmentPressed(_ sender: UIButton) {
-        setupBlurredBackgroundView()
-    }
+  // MARK: - Action Methods
   
-    // MARK: - Properties
+  @IBAction func addAssignmentPressed(_ sender: UIButton) {
+      setupBlurredBackgroundView()
+  }
   
-    var student: Student!
-    var activeTextField = UITextField()
-    var indexPathForEditRow: IndexPath!
-    var correctAssignments = [Assignment]()
+  // MARK: - Properties
+  
+  var student: Student!
+  var activeTextField = UITextField()
+  var indexPathForEditRow: IndexPath!
+  var correctAssignments = [Assignment]()
+  let noDataLabel = UILabel()
 
-    // MARK: - Life Cycle
+  // MARK: - Life Cycle
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadStudent()
-        setupUI()
-        loadStudentAssignments()
-    }
-  
-     override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
-    }
-  
-    // MARK: - Custom Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    private func loadStudent() {
-        nameLabel.text = student.name
-        subjectLabel.text = student.subject
-        studentImageView.image = student.image
+    loadStudent()
+    setupUI()
+    loadStudentAssignments()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+      NotificationCenter.default.removeObserver(self)
+  }
+  
+
+  
+  // MARK: - Custom Methods
+  
+  private func loadStudent() {
+    nameLabel.text = student.name
+    subjectLabel.text = student.subject
+    studentImageView.image = student.image
+  }
+  
+  private func loadStudentAssignments() {
+    let activeTeacher = ActiveUser.shared.current as! Teacher
+    guard let recordName = activeTeacher.record?.recordID.recordName else { fatalError() }
+    student.filterAssignments(by: activeTeacher)
+    let assignmentsDict = student.teacherAssignmentsDict
+    correctAssignments = assignmentsDict[recordName] ?? [Assignment]()
+    assignmentsTableView.reloadData()
+  }
+  
+  private func setupUI() {
+    
+    // MARK: - Navigation Bar
+    
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    self.navigationController?.navigationBar.shadowImage = UIImage()
+    
+    // MARK: - Table View
+    
+    assignmentsTableView.tableFooterView = UIView(frame: .zero)
+  }
+  
+  
+  func setupAlert(indexPath: IndexPath) {
+    let alert = UIAlertController(title: "Edit", message: "Please edit", preferredStyle: .alert)
+    
+    let edit = UIAlertAction(title: "Edit", style: .default) { (alertAction) in
+      let textField = alert.textFields![0] as UITextField
+      self.correctAssignments[indexPath.row].assignmentTitle = textField.text!
+      self.assignmentsTableView.reloadData()
+      let titleTextField = alert.textFields![0] as UITextField
+      let descriptionTextField = alert.textFields![1] as UITextField
+      
+      self.correctAssignments[indexPath.row].assignmentTitle = titleTextField.text!
+      self.correctAssignments[indexPath.row].assignmentDescription = descriptionTextField.text!
+      
+      self.assignmentsTableView.reloadData()
     }
     
-    private func loadStudentAssignments() {
-        let activeTeacher = ActiveUser.shared.current as! Teacher
-        guard let recordName = activeTeacher.record?.recordID.recordName else { fatalError() }
-        student.filterAssignments(by: activeTeacher)
-        let assignmentsDict = student.teacherAssignmentsDict
-        correctAssignments = assignmentsDict[recordName] ?? [Assignment]()
-        assignmentsTableView.reloadData()
+    alert.addTextField { (textField) in
+      textField.text = self.correctAssignments[indexPath.row].assignmentTitle
+    }
+    alert.addTextField { (textField) in
+      textField.text = self.correctAssignments[indexPath.row].assignmentDescription
     }
     
-    private func setupUI() {
-        
-        // MARK: - Navigation Bar
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        
-        // MARK: - Table View
-        
-        assignmentsTableView.tableFooterView = UIView(frame: .zero)
-    }
+    let dismiss = UIAlertAction(title: "Dismiss", style: .destructive)
+    
+    alert.addAction(dismiss)
+    alert.addAction(edit)
+    
+    self.present(alert, animated: true)
+  }
+  
+  private func setupBlurredBackgroundView() {
+    let blurredBackgroundView = UIVisualEffectView()
+    blurredBackgroundView.frame = view.frame
+    blurredBackgroundView.effect = UIBlurEffect(style: .dark)
+    view.addSubview(blurredBackgroundView)
+  }
+  
+  private func setTextForEmptyTableView() {
+    let attributedString = NSMutableAttributedString(string: "Press  ")
+    let addDescImageAttachment = NSTextAttachment()
+    addDescImageAttachment.image = UIImage(named: "add-desc-selected")
+    addDescImageAttachment.bounds = CGRect(x: 0, y: -5, width: 20, height: 20)
+    attributedString.append(NSAttributedString(attachment: addDescImageAttachment))
+    attributedString.append(NSAttributedString(string: "  To Add A New Assignment!"))
+    
+    noDataLabel.attributedText = attributedString
+    noDataLabel.font = UIFont(name: "Dosis", size: 17)
+    noDataLabel.textColor = UIColor.black
+    noDataLabel.textAlignment = .center
+    assignmentsTableView.addSubview(noDataLabel)
+    noDataLabel.translatesAutoresizingMaskIntoConstraints = false
+    noDataLabel.centerYAnchor.constraint(equalTo: assignmentsTableView.centerYAnchor).isActive = true
+    noDataLabel.centerXAnchor.constraint(equalTo: assignmentsTableView.centerXAnchor).isActive = true
     
 
-    func setupAlert(indexPath: IndexPath) {
-        let alert = UIAlertController(title: "Edit", message: "Please edit", preferredStyle: .alert)
-      
-        let edit = UIAlertAction(title: "Edit", style: .default) { (alertAction) in
-            let textField = alert.textFields![0] as UITextField
-            self.correctAssignments[indexPath.row].assignmentTitle = textField.text!
-            self.assignmentsTableView.reloadData()
-            let titleTextField = alert.textFields![0] as UITextField
-            let descriptionTextField = alert.textFields![1] as UITextField
-          
-            self.correctAssignments[indexPath.row].assignmentTitle = titleTextField.text!
-            self.correctAssignments[indexPath.row].assignmentDescription = descriptionTextField.text!
-          
-            self.assignmentsTableView.reloadData()
-        }
-      
-        alert.addTextField { (textField) in
-            textField.text = self.correctAssignments[indexPath.row].assignmentTitle
-        }
-        alert.addTextField { (textField) in
-            textField.text = self.correctAssignments[indexPath.row].assignmentDescription
-        }
-      
-        let dismiss = UIAlertAction(title: "Dismiss", style: .destructive)
-      
-        alert.addAction(dismiss)
-        alert.addAction(edit)
-      
-        self.present(alert, animated: true)
-    }
+  }
   
-    private func setupBlurredBackgroundView() {
-        let blurredBackgroundView = UIVisualEffectView()
-        blurredBackgroundView.frame = view.frame
-        blurredBackgroundView.effect = UIBlurEffect(style: .dark)
-        view.addSubview(blurredBackgroundView)
+  func updateTextForEmptyTableView() {
+    if correctAssignments.count > 0 {
+      assignmentsTableView.backgroundView = nil
+      assignmentsTableView.separatorStyle = .singleLine
+      noDataLabel.isHidden = true
+    } else {
+      noDataLabel.isHidden = false
     }
+  }
   
-    //MARK: - Segue
+  //MARK: - Segue
   
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "addAssignmentSegue" {
-            if let addAssignmentVC = segue.destination as? AddAssignmentViewController {
-                addAssignmentVC.delegate = self
-                addAssignmentVC.modalPresentationStyle = .overFullScreen
-                self.navigationController?.isNavigationBarHidden = true
-            }
-        }
-      
-        if segue.identifier == "editAssignmentSegue" {
-            if let editAssignmentVC = segue.destination as? EditAssignmentViewController {
-                editAssignmentVC.delegate = self
-                let currentAssignment = student.assignments[indexPathForEditRow.row]
-                editAssignmentVC.assignment = currentAssignment
-                editAssignmentVC.modalPresentationStyle = .overFullScreen
-                self.navigationController?.isNavigationBarHidden = true
-            }
-        }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    if segue.identifier == "addAssignmentSegue" {
+      if let addAssignmentVC = segue.destination as? AddAssignmentViewController {
+        addAssignmentVC.delegate = self
+        self.navigationController?.isNavigationBarHidden = true
+
+        addAssignmentVC.modalPresentationStyle = .overFullScreen
+      }
     }
+    
+    if segue.identifier == "editAssignmentSegue" {
+      if let editAssignmentVC = segue.destination as? EditAssignmentViewController {
+        editAssignmentVC.delegate = self
+        let currentAssignment = student.assignments[indexPathForEditRow.row]
+        editAssignmentVC.assignment = currentAssignment
+        self.navigationController?.isNavigationBarHidden = true
+
+        editAssignmentVC.modalPresentationStyle = .overFullScreen
+      }
+    }
+  }
 }
 
 // MARK: - Table View Data Source
 
 extension StudentDetailViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        if correctAssignments.count > 0 {
-            tableView.backgroundView = nil
-            tableView.separatorStyle = .singleLine
-        } else {
-            // text for empty table view
-            let noDataLabel = UILabel()
-            let attributedString = NSMutableAttributedString(string: "Press  ")
-            let addDescImageAttachment = NSTextAttachment()
-            addDescImageAttachment.image = UIImage(named: "add-desc-selected")
-            addDescImageAttachment.bounds = CGRect(x: 0, y: -5, width: 20, height: 20)
-            attributedString.append(NSAttributedString(attachment: addDescImageAttachment))
-            attributedString.append(NSAttributedString(string: "  To Add A New Assignment!"))
-            noDataLabel.attributedText = attributedString
-          
-            noDataLabel.font = UIFont(name: "Dosis", size: 17)
-            noDataLabel.textColor = UIColor.black
-            noDataLabel.textAlignment = .center
-            tableView.backgroundView = noDataLabel
-            tableView.separatorStyle = .none
-        }
-        return 1
-    }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    updateTextForEmptyTableView()
+    return 1
+  }
   
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return correctAssignments.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return correctAssignments.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "assignmentCell", for: indexPath) as! AssignmentCell
-      
-        let assignment = correctAssignments[indexPath.row]
-        cell.configureCellWith(assignment: assignment)
-      
-        return cell
-    }
+    let cell = tableView.dequeueReusableCell(withIdentifier: "assignmentCell", for: indexPath) as! AssignmentCell
+    
+    let assignment = correctAssignments[indexPath.row]
+    cell.configureCellWith(assignment: assignment)
+    setTextForEmptyTableView()
+    return cell
+  }
 }
+
+
+
 
 // MARK: - Table View Delegate
 
 extension StudentDetailViewController: UITableViewDelegate {
   
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-      
-        if editingStyle == .delete {
-            correctAssignments.remove(at: indexPath.row)
-            assignmentsTableView.deleteRows(at: [indexPath], with: .fade)
-        }
+//  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//
+//    if editingStyle == .delete {
+//      correctAssignments.remove(at: indexPath.row)
+//      assignmentsTableView.deleteRows(at: [indexPath], with: .fade)
+//    }
+//  }
+//
+  func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+      self.correctAssignments.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+      tableView.reloadData()
     }
-
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
-            self.correctAssignments.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-      
-        let edit = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexPath) in
-            self.indexPathForEditRow = indexPath
-            self.setupBlurredBackgroundView()
-            self.performSegue(withIdentifier: "editAssignmentSegue", sender: nil)
-        }
-      
-        edit.backgroundColor = UIColor.blue
-      
-        return [delete, edit]
+    
+    let edit = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexPath) in
+      self.indexPathForEditRow = indexPath
+      self.setupBlurredBackgroundView()
+      self.performSegue(withIdentifier: "editAssignmentSegue", sender: nil)
     }
+    
+    edit.backgroundColor = UIColor.blue
+    
+    return [delete, edit]
+  }
   
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath) as! AssignmentCell
-        cell.toggle()
-        tableView.reloadData()
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let cell = tableView.cellForRow(at: indexPath) as! AssignmentCell
+    cell.toggle()
+    tableView.reloadData()
+  }
+  
+  
 }
 
 // MARK: - AddAssignmentController Delegate Methods
 
 extension StudentDetailViewController: AddAssignmentControllerDelegate {
-
-    func removeBlurredBackgroundView() {
-        for subview in view.subviews {
-            if subview.isKind(of: UIVisualEffectView.self) {
-                subview.removeFromSuperview()
-            }
-        }
-        self.navigationController?.isNavigationBarHidden = false
-    }
   
-    func addAssignment(newAssignment: Assignment) {
-        correctAssignments.insert(newAssignment, at: 0)
-        let teacher = ActiveUser.shared.current as! Teacher
-        let indexPath = IndexPath(row: 0, section: 0)
-        assignmentsTableView.insertRows(at: [indexPath], with: .automatic)
-        CloudKitManager.add(assignment: newAssignment, from: teacher, to: student) { (records) in
-            guard let records = records else { fatalError() }
-            self.student.record = records.filter { $0.recordType == "Students" }.first!
-            DispatchQueue.main.async {
-                self.assignmentsTableView.reloadData()
-            }
-        }
+  func removeBlurredBackgroundView() {
+    for subview in view.subviews {
+      if subview.isKind(of: UIVisualEffectView.self) {
+        subview.removeFromSuperview()
+      }
     }
+    self.navigationController?.isNavigationBarHidden = false
+  }
+  
+  func addAssignment(newAssignment: Assignment) {
+    correctAssignments.insert(newAssignment, at: 0)
+    let teacher = ActiveUser.shared.current as! Teacher
+    let indexPath = IndexPath(row: 0, section: 0)
+    assignmentsTableView.insertRows(at: [indexPath], with: .automatic)
+    CloudKitManager.add(assignment: newAssignment, from: teacher, to: student) { (records) in
+      guard let records = records else { fatalError() }
+      self.student.record = records.filter { $0.recordType == "Students" }.first!
+      DispatchQueue.main.async {
+        self.assignmentsTableView.reloadData()
+      }
+    }
+  }
   
 }
 
@@ -247,21 +273,28 @@ extension StudentDetailViewController: AddAssignmentControllerDelegate {
 
 extension StudentDetailViewController: EditAssignmentControllerDelegate {
   
-    func editAssignment(editedAssignment: Assignment) {
-        student.assignments[self.indexPathForEditRow.row] = editedAssignment
-        assignmentsTableView.reloadData()
-      
-//        student.assignments.insert(editedAssignment, at: 0)
-//        let teacher = ActiveUser.shared.current as! Teacher
-//        let indexPath = IndexPath(row: 0, section: 0)
-//        assignmentsTableView.insertRows(at: [indexPath], with: .automatic)
-//        CloudKitManager.add(assignment: editedAssignment, from: teacher, to: student) { (records) in
-//            guard let records = records else { fatalError() }
-//            self.student.record = records.filter { $0.recordType == "Students" }.first!
-//            DispatchQueue.main.async {
-//                self.assignmentsTableView.reloadData()
-//            }
-//        }
-    }
+  func editAssignment(editedAssignment: Assignment) {
+    student.assignments[self.indexPathForEditRow.row] = editedAssignment
+    assignmentsTableView.reloadData()
+    
+    //        student.assignments.insert(editedAssignment, at: 0)
+    //        let teacher = ActiveUser.shared.current as! Teacher
+    //        let indexPath = IndexPath(row: 0, section: 0)
+    //        assignmentsTableView.insertRows(at: [indexPath], with: .automatic)
+    //        CloudKitManager.add(assignment: editedAssignment, from: teacher, to: student) { (records) in
+    //            guard let records = records else { fatalError() }
+    //            self.student.record = records.filter { $0.recordType == "Students" }.first!
+    //            DispatchQueue.main.async {
+    //                self.assignmentsTableView.reloadData()
+    //            }
+    //        }
+  }
   
+}
+
+
+extension StudentDetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        setTextForEmptyTableView()
+    }
 }
--- TutorApp/Views/AssignmentCell.swift
+++ TutorApp/Views/AssignmentCell.swift
@@ -11,25 +11,33 @@ import Foundation
 
 class AssignmentCell: UITableViewCell {
 
+    // MARK: - Outlets
+  
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+  
+    // MARK: - Properties
+  
     var isExpanded: Bool = false
+
+    // MARK: - Methods
   
     func configureCellWith(assignment: Assignment) {
-      titleLabel.text = assignment.assignmentTitle
-      descriptionLabel.text = self.isExpanded ? assignment.assignmentDescription : ""
-      if isExpanded {
-        backgroundColor = #colorLiteral(red: 0.1067340448, green: 0.4299619794, blue: 0.02381768264, alpha: 1)
-        titleLabel.textColor = UIColor.white
-        descriptionLabel.textColor = UIColor.white
-      } else {
-        backgroundColor = UIColor.white
-        titleLabel.textColor = UIColor.darkGray
-        descriptionLabel.textColor = UIColor.darkGray
-      }
+      
+        titleLabel.text = assignment.assignmentTitle
+        descriptionLabel.text = self.isExpanded ? assignment.assignmentDescription : ""
+        if isExpanded {
+            self.backgroundColor = #colorLiteral(red: 0.1067340448, green: 0.4299619794, blue: 0.02381768264, alpha: 1)
+            titleLabel.textColor = UIColor.white
+            descriptionLabel.textColor = UIColor.white
+        } else {
+            self.backgroundColor = UIColor.white
+            titleLabel.textColor = UIColor.darkGray
+            descriptionLabel.textColor = UIColor.darkGray
+        }
     }
   
     func toggle() {
-      isExpanded = !isExpanded
+        isExpanded = !isExpanded
     }
 }
